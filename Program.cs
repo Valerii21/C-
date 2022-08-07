ac1a5253441603ac2538d99873539183e1a7a26a
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-08-07 13:06:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+            int num_A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            int num_B = Convert.ToInt32(Console.ReadLine());
+
+            if (num_A > num_B)
+            {
+                Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+            }
+
+    
